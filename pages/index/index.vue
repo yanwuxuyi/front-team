e269a4da6c0480ad3215c00ac8517e8e15be6e77
xvpi <904017172@qq.com>
2024-07-05 13:59:11 +0800

    t070501

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,21 +1,19 @@
 <template>
 	<view class="u-wrap">
 		<view>
-			<u-navbar title-color="#fff" back-icon-color="#ffffff"
-				:is-fixed="isFixed" :is-back="isBack" 
-				:background="background" 
-				:back-text-style="{color: '#fff'}" :title="title1" 
+			<u-navbar title-color="#fff" back-icon-color="#ffffff" :is-fixed="isFixed" :is-back="isBack"
+				:background="background" :back-text-style="{color: '#fff'}" :title="title1"
 				:back-icon-name="backIconName" :back-text="backText">
 				<view class="slot-wrap">
-				<view class="map-wrap" v-if="custom">
-					<u-icon name="map" color="#ffffff" size="24"></u-icon>
-					<text class="map-wrap-text">重庆大学虎溪校区</text>
+					<view class="map-wrap" v-if="custom">
+						<u-icon name="map" color="#ffffff" size="24"></u-icon>
+						<text class="map-wrap-text">重庆大学虎溪校区</text>
+					</view>
 				</view>
-				</view>
-				  <view class="right-slot">
+				<view class="right-slot">
 					<text class="weather-text">{{currentWeather}}</text>
 					<image :src="weatherIcon" class="weather-icon" @click="showWeather"></image>
-				  </view>
+				</view>
 			</u-navbar>
 		</view>
 		<view>
@@ -23,40 +21,19 @@
 		</view>
 		<view 滚动通知 class="center">
 			<u-toast :type="type" ref="uToast"></u-toast>
-			<u-notice-bar 
-			:autoplay="true" 
-			:playState="play" 
-			:speed="160"
-			:mode="horizontal" 
-			:type="warning" 
-			:list="infoList"
-			:moreIcon="false" 
-			:volumeIcon="true" 
-			:duration="2000" 
-			:isCircular="true">
+			<u-notice-bar :autoplay="true" :playState="play" :speed="160" :mode="horizontal" :type="warning"
+				:list="infoList" :moreIcon="false" :volumeIcon="true" :duration="2000" :isCircular="true">
 			</u-notice-bar>
 		</view>
 		<view 倒计时 class="center" style="background-color: #fcffd3;">
 			<text>\n距离报道截止还有：</text>
-			<u-count-down 
-			:timestamp="timeStamp" 
-			:separator="separator" 
-			:showDays="false"
-			:separator-color="separatorColor" 
-			:fontSize="30" 
-			:color="color"  
-			bg-color="rgb(250, 250, 250)"
-			@end ="end">
+			<u-count-down :timestamp="timeStamp" :separator="separator" :showDays="false"
+				:separator-color="separatorColor" :fontSize="30" :color="color" bg-color="rgb(250, 250, 250)"
+				@end="end">
 			</u-count-down>
 			<text>\n已经报道</text>
-			<u-count-to
-				:autoplay="true"
-				:startVal="0"
-				:endVal="endVal"
-				:duration="10000"
-				:bold="true"
-				style="fontSize:130%"
-			></u-count-to> 
+			<u-count-to :autoplay="true" :startVal="0" :endVal="endVal" :duration="10000" :bold="true"
+				style="fontSize:130%"></u-count-to>
 			<text>人\n\n</text>
 		</view>
 		<view>
@@ -64,12 +41,13 @@
 				<uni-popup-share title="小功能"></uni-popup-share>
 			</uni-popup>
 		</view>
-		<view >
+		<view>
 			<u-row :justify="justify" @click="rowClick">
 				<u-col :span="span" :offset="offset" stop>
-					<u-card style="background-color: #65d1ff;" class="style1" @click="goInform()" :title="titley" :thumb="thumby" :padding="paddingy" :border="bordery"
-						:head-border-bottom="hbby" :foot-border-top="fbty">
-						<view  class="" slot="body">
+					<u-card style="background-color: #65d1ff;" class="style1" @click="goInform()" :title="titley"
+						:thumb="thumby" :padding="paddingy" :border="bordery" :head-border-bottom="hbby"
+						:foot-border-top="fbty">
+						<view class="" slot="body">
 							<view class="u-body-item u-flex u-border-bottom u-col-between u-p-t-0">
 								<view class="u-body-item-title u-line-2">
 									请认真查看报道须知
@@ -79,8 +57,9 @@
 					</u-card>
 				</u-col>
 				<u-col :span="span" :offset="offset" @click="click" stop>
-					<u-card style="background-color: #c4ff97;" class="style1" @click="goFlow()" :title="titlez" :thumb="thumbz" :padding="paddingz" :border="borderz"
-						:head-border-bottom="hbbz" :foot-border-top="fbtz">
+					<u-card style="background-color: #c4ff97;" class="style1" @click="goFlow()" :title="titlez"
+						:thumb="thumbz" :padding="paddingz" :border="borderz" :head-border-bottom="hbbz"
+						:foot-border-top="fbtz">
 						<view class="" slot="body">
 							<view class="u-body-item u-flex u-border-bottom u-col-between u-p-t-0">
 								<view class="u-body-item-title u-line-2">
@@ -91,8 +70,9 @@
 					</u-card>
 				</u-col>
 				<u-col :span="span" :offset="offset" @click="click" stop>
-					<u-card style="background-color: #ffd249;" class="style1" @click="goCollect()" :title="titlex" :thumb="thumbx" :padding="paddingx" :border="borderx"
-						:head-border-bottom="hbbx" :foot-border-top="fbtx">
+					<u-card style="background-color: #ffd249;" class="style1" @click="goCollect()" :title="titlex"
+						:thumb="thumbx" :padding="paddingx" :border="borderx" :head-border-bottom="hbbx"
+						:foot-border-top="fbtx">
 						<view class="" slot="body">
 							<view class="u-body-item u-flex u-border-bottom u-col-between u-p-t-0">
 								<view class="u-body-item-title u-line-2">
@@ -103,8 +83,9 @@
 					</u-card>
 				</u-col>
 				<u-col :span="span" :offset="offset" @click="click" stop>
-					<u-card style="background-color: #ffc69c;" class="style1" @click="goReport()" :title="titlev" :thumb="thumbv" :padding="paddingz" :border="borderz"
-						:head-border-bottom="hbbz" :foot-border-top="fbtz">
+					<u-card style="background-color: #ffc69c;" class="style1" @click="goReport()" :title="titlev"
+						:thumb="thumbv" :padding="paddingz" :border="borderz" :head-border-bottom="hbbz"
+						:foot-border-top="fbtz">
 						<view class="" slot="body">
 							<view class="u-body-item u-flex u-border-bottom u-col-between u-p-t-0">
 								<view class="u-body-item-title u-line-2">
@@ -116,17 +97,44 @@
 				</u-col>
 			</u-row>
 		</view>
-				<view @click="goAnotherPage" class="floating-icon">
-					<u-icon name="chat" size="40" color="#c7ddff"></u-icon>
-				</view>
+		<view @click="goAnotherPage" class="floating-icon">
+			<u-icon name="chat" size="40" color="#c7ddff"></u-icon>
+		</view>
+		<view class="step-container">
+			<view v-for="(step, index) in steps" :key="index" class="step-item"
+				:class="{'active': index === currentStep - 1, 'completed': index < currentStep-1}">
+				<text>{{ step.label }}</text>
+				<view v-if="index === currentStep-1" class="step-dot active"></view>
+				<view v-else class="step-dot"></view>
+			</view>
+		</view>
 	</view>
+
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				currentWeather:'',
+				steps: [{
+						label: '学院报道'
+					},
+					{
+						label: '住宿办理'
+					},
+					{
+						label: '财务办理'
+					},
+					{
+						label: '档案管理'
+					},
+					{
+						label: '医院体检'
+					}
+				],
+				currentStep: 0, // 初始步骤  
+				studentId: '',
+				currentWeather: '',
 				// 倒计时
 				timeStamp: '86400',
 				// 滚动数字
@@ -166,7 +174,7 @@
 				duration: 2000,
 				// 倒计时
 				separator: 'zh',
-				color: '#00aa7f',  
+				color: '#00aa7f',
 				separatorColor: '#02140b',
 				// 滚动通知
 				warning: 'warning',
@@ -196,10 +204,10 @@
 						selectedIconPath: "account-fill",
 						customIcon: false,
 					},
-					],
-					inactiveColor: '#bababa',
-					activeColor: '#fff2d7',
-					// 模块列表
+				],
+				inactiveColor: '#bababa',
+				activeColor: '#fff2d7',
+				// 模块列表
 				slideshowlist: [{
 						image: '/static/images/slideshowpic/1.jpg',
 						title: '迎新大会'
@@ -240,35 +248,38 @@
 			}
 		},
 		computed: {
-		    weatherIcon() {
-				console.log('选择：'+ this.currentWeather);
-		      switch (this.currentWeather) {
-		        case '晴':
-		          return '../../static/icon/sun.png';
-				case '大雨':
-				    return '../../static/icon/bigrain.png';
-				case '大暴雨':
-				    return '../../static/icon/bigrain.png';
-				case '雷阵雨':
-					return '../../static/icon/rerain.png';
-		        case '中雨':
-		          return '../../static/icon/rain.png';
-				case '小雨':
-				    return '../../static/icon/rain.png';
-		        case '多云':
-		          return '../../static/icon/cloud.png';
-		        default:
-		          return '../../static/icon/cloud.png';
-		      }
-		    },
-		  },
+			weatherIcon() {
+				console.log('选择：' + this.currentWeather);
+				switch (this.currentWeather) {
+					case '晴':
+						return '../../static/icon/sun.png';
+					case '大雨':
+						return '../../static/icon/bigrain.png';
+					case '大暴雨':
+						return '../../static/icon/bigrain.png';
+					case '雷阵雨':
+						return '../../static/icon/rerain.png';
+					case '中雨':
+						return '../../static/icon/rain.png';
+					case '小雨':
+						return '../../static/icon/rain.png';
+					case '多云':
+						return '../../static/icon/cloud.png';
+					default:
+						return '../../static/icon/cloud.png';
+				}
+			},
+		},
+		onShow() {
+			this.initStatus();
+		},
 		onLoad() {
 			uni.request({
-				url:'http://192.168.50.101:8090/count/countRegisterStudent',
+				url: 'http://192.168.50.101:8090/count/countRegisterStudent',
 				success: (res) => {
 					console.log(res);
-					if(res.statusCode = 200){
-					this.endVal = res.data.result.count;
+					if (res.statusCode = 200) {
+						this.endVal = res.data.result.count;
 					}
 				}
 			})
@@ -281,15 +292,15 @@
 			// uni.navigateTo({
 			// 	url: '/pages/frmv/webView?url=' + 'https://v.xiumi.us/stage/v5/6q26F/465620607'
 			// })
-			
+
 			// uni.navigateTo({
 			// 	url: '/pages/frmv/webrouterspecial?url=' + encodeURIComponent('https://v.xiumi.us/stage/v5/6q26F/465620607')
 			// })
 			uni.request({
 				url: 'http://192.168.50.101:8090/utils/weather',
-				success: (res) =>{
+				success: (res) => {
 					console.log(res);
-					if(res.statusCode = 200){
+					if (res.statusCode = 200) {
 						this.currentWeather = res.data.showapi_res_body.hourList[0].weather;
 						console.log(this.currentWeather);
 					}
@@ -297,18 +308,55 @@
 			})
 			this.calculateTime();
 		},
-		
+
 		methods: {
+			initStatus() {
+				const value = uni.getStorageSync('user');
+				if (value && value.studentId) {
+					this.studentId = value.studentId;
+					// 假设 getRegisterStatus 是一个异步函数，这里用 setTimeout 模拟异步调用  
+					setTimeout(() => {
+						// 假设 getRegisterStatus 返回当前步骤索引（0-4）  
+						const registerStatus = this.getRegisterStatus(this.studentId) - 1; // 你需要实现这个函数  
+						this.currentStep = registerStatus;
+					}, 10);
+				}
+			},
+			getRegisterStatus(studentId) {
+				uni.request({
+					url: 'http://192.168.50.101:8090/personalRegister/getRegisterStatus',
+					method: 'GET',
+					data: {
+						studentid: this.studentId
+					},
+					success: (res) => {
+						if (res.data.status === true) {
+							// 解析后端返回的数据
+							this.currentStep = JSON.parse(res.data.result);
+							console.log('目前状态数据:', this.currentStep);
+						} else {
+							console.error('请求失败:', res);
+						}
+					},
+					fail: (err) => {
+						uni.showToast({
+							title: '获取当前报道注册状态失败',
+							icon: 'none'
+						})
+					}
+				});
+				return res.data.result;
+			},
 			showWeather() {
 				uni.navigateTo({
 					url: '../index/weather' // 替换为你实际的页面路径
 				});
 			},
 			goAnotherPage() {
-							uni.navigateTo({
-								url: '../index/test2' // 替换为你实际的页面路径
-							});
-						},
+				uni.navigateTo({
+					url: '../index/test2' // 替换为你实际的页面路径
+				});
+			},
 			imageError() {
 
 			},
@@ -343,8 +391,7 @@
 					uni.navigateTo({
 						url: '../frmv/collect'
 					})
-				}
-				else {
+				} else {
 					this.showToast()
 				}
 			},
@@ -354,8 +401,7 @@
 					uni.navigateTo({
 						url: '../frmv/stepOfReport'
 					})
-				}
-				else {
+				} else {
 					this.showToast()
 				}
 			},
@@ -370,45 +416,92 @@
 				});
 			},
 			calculateTime() {
-            var targetTimestamp = 1689070000000;
-            var currentDate = new Date(); // 获取当前日期
-			var currentTimestamp = currentDate.getTime()
-            var timeDiff = targetTimestamp - currentTimestamp; // 计算目标日期和当前日期的时间差（毫秒）
-			this.timeStamp = Math.floor(timeDiff / 1000); // 将时间差转换为秒数
+				var targetTimestamp = 1689070000000;
+				var currentDate = new Date(); // 获取当前日期
+				var currentTimestamp = currentDate.getTime()
+				var timeDiff = targetTimestamp - currentTimestamp; // 计算目标日期和当前日期的时间差（毫秒）
+				this.timeStamp = Math.floor(timeDiff / 1000); // 将时间差转换为秒数
 			}
 		}
 	}
 </script>
 
 <style scoped lang="scss">
+	.step-container {
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		padding: 20px;
+	}
+
+	.step-item {
+		text-align: center;
+		position: relative;
+		flex: 1;
+	}
+
+	.step-item text {
+		color: #999;
+	}
+
+	.step-item.active text,
+	.step-item.completed text {
+		color: #333;
+		font-weight: bold;
+	}
+
+	.step-dot {
+		height: 20px;
+		width: 20px;
+		border-radius: 50%;
+		background-color: #ccc;
+		position: absolute;
+		left: 50%;
+		transform: translateX(-50%);
+		bottom: -25px;
+	}
+
+	.step-dot.active {
+		background-color: #409EFF;
+	}
+
+	.step-item.completed .step-dot {
+		background-color: #2ECC71;
+	}
+
 	.floating-icon {
-			position: fixed;
-			bottom: 80px;
-			right: 10px;
-			width: 50px;
-			height: 50px;
-			background-color: #007aff;
-			border-radius: 50%;
-			display: flex;
-			align-items: center;
-			justify-content: center;
-			box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
-			z-index: 1000;
-			cursor: pointer;
-		}
+		position: fixed;
+		bottom: 80px;
+		right: 10px;
+		width: 50px;
+		height: 50px;
+		background-color: #007aff;
+		border-radius: 50%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+		z-index: 1000;
+		cursor: pointer;
+	}
+
 	.center {
 		justify-content: center;
 		text-align: center;
 	}
-	.style1{
+
+	.style1 {
 		width: 100%;
 	}
-	.text{
+
+	.text {
 		font-size: 40rpx;
 	}
+
 	.wrap {
-			padding: 40rpx;
-		}
+		padding: 40rpx;
+	}
+
 	.u-demo {
 		padding-top: 0;
 	}
@@ -433,47 +526,53 @@
 		border-radius: 8rpx;
 		margin-left: 12rpx;
 	}
-	
+
 	.slot-wrap {
 		display: flex;
 		align-items: center;
 		flex: 1;
 	}
+
 	.weather-icon {
-	  width: 30px;
-	  height: 30px;
-	  cursor: pointer;
+		width: 30px;
+		height: 30px;
+		cursor: pointer;
 	}
+
 	.map-wrap {
 		display: flex;
 		align-items: center;
 		padding: 4px 6px;
-		background-color: rgba(240,240, 240, 0.35);
+		background-color: rgba(240, 240, 240, 0.35);
 		color: #fff;
 		font-size: 22rpx;
 		border-radius: 100rpx;
 		margin-left: 30rpx;
 	}
-	
+
 	.buttonStyle {
 		width: 130rpx;
 		height: 130rpx;
 		top: 70rpx;
 	}
-	
+
 	.popupStyle {
 		color-scheme: #352f7f;
 		border-color: #352f7f;
 		color: #352f7f;
 	}
+
 	.weather-text {
-	  margin-right: 6px; /* 控制文本和图标之间的间距 */
-	  color: #ffffff; /* 设置文本颜色 */
-	}
-	.right-slot {
-	  display: flex;
-	  align-items: center;
-	  margin-right: 10px; /* 添加右边距，控制图标位置 */
+		margin-right: 6px;
+		/* 控制文本和图标之间的间距 */
+		color: #ffffff;
+		/* 设置文本颜色 */
 	}
 
+	.right-slot {
+		display: flex;
+		align-items: center;
+		margin-right: 10px;
+		/* 添加右边距，控制图标位置 */
+	}
 </style>
