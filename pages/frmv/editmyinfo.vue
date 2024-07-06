4bbf0dc6f1950cf62a2c254b268270613f464d7b
hlzangbang <20221334@stu.cqu.edu.cn>
2024-07-06 21:07:03 +0800

    test

--- pages/frmv/editmyinfo.vue
+++ pages/frmv/editmyinfo.vue
@@ -271,8 +271,7 @@
 				],
 				actionSheetList: [
 
-<<<<<<< HEAD
-					{
+                   {
 						text: '男'
 					},
 					{
@@ -622,12 +621,4 @@
 	  vertical-align: middle;
 	}
 
-=======
-<style>
-	.wrap{
-		padding: 10rpx 50rpx;
-		width: 100%;
-		height: 100%;
-	}
->>>>>>> 48ed95536c6adfe2cb17d1a272d26fa8279f9ef9
 </style>
